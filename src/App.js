4db046470802848b96e9574fc35dba54f397d8fa
Tran Quoc Duy <duyb2110270@student.ctu.edu.vn>
2024-08-27 14:15:17 +0700

    update drag and drop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import './App.scss'
 import Auth from './Auth/Auth'
+import Home from './components/Home/Home';
 
 const App = () => {
   const [Login, SetLogin] = useState(false);
   const handleDataFromChild = (data) => {
-    // console.log(data);
     if(data.email){
       SetLogin(true);
     }
   };
   return (
     <div className='Background'>
-      {Login ? <div/> : <Auth onData={handleDataFromChild}/>}
+      {Login ? <Home/> : <Auth onData={handleDataFromChild}/>}
     </div>
   )
 }
